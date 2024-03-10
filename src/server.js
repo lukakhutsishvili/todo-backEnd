d2ca41fa9da6f5990ea9c26c6cdc47312fb09f39
Luka Khutsishvili <luka.khutsishvili777@gmail.com>
2024-03-10 06:43:41 +0400

    add cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import bodyParser from "body-parser";
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import cors from "cors";
 
 //rato vwert ./?
-import connectToMongo from './config/mongo.js'
+import connectToMongo from "./config/mongo.js";
 import todoRouter from "./routes/todoRouter.js";
 import SwaggerMiddleware from "./middlewares/swagger-middleware.js";
 
@@ -12,8 +13,9 @@ dotenv.config();
 connectToMongo();
 
 app.use(bodyParser.json());
+app.use(cors());
 
-app.use("/api", todoRouter)
+app.use("/api", todoRouter);
 app.use("/", ...SwaggerMiddleware());
 
 app.listen(process.env.PORT || 3000);
